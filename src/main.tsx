0fc26cd0d42ed705305d867b8a76e266ad97c79b
hammouda msaad <hammouda.msaad@esprit.tn>
2025-05-29 18:17:01 +0100

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+document.addEventListener('contextmenu', e => e.preventDefault());
+document.addEventListener('copy', e => e.preventDefault());
+document.addEventListener('keydown', e => {
+  if (
+    e.key === 'F12' ||
+    (e.ctrlKey && ['U', 'u', 'S', 's', 'C', 'c'].includes(e.key)) ||
+    (e.ctrlKey && e.shiftKey && ['I', 'i', 'J', 'j'].includes(e.key))
+  ) {
+    e.preventDefault();
+  }
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
